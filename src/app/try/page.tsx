7b8bb8a2dc39de32575cab496e32a58e6c05f0d8
Nikolai Schunk <58025175+nikolaischunk@users.noreply.github.com>
2024-02-13 16:37:20 +0100

    Add Button Variants & remove unnecessary deps

--- src/app/try/page.tsx
+++ src/app/try/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/ui/button';
+import { H1 } from '@/ui/typography';
 
 const Tryout: React.FC = () => {
   return (
@@ -6,7 +7,23 @@ const Tryout: React.FC = () => {
       <div className="container">
         <h1 className="font-heading">Hello World H1</h1>
         <p className="font-body">Hello World Text</p>
-        <Button>Primary</Button>
+        <H1>Hello World H1</H1>
+        <H1>Buttons</H1>
+        <div className="buttonGroup grid grid-cols-6 gap-3">
+          <Button>Primary</Button>
+          <Button variant={'secondary'}>Secondary</Button>
+          <Button variant={'destructive'}>Destructive</Button>
+          <Button variant={'ghost'}>Ghost</Button>
+          <Button variant={'link'}>Link</Button>
+          <Button variant={'outline'}>Outline</Button>
+          <Button variant={'expandIcon'}>Expand Icon</Button>
+          <Button variant={'ringHover'}>Ring Hover</Button>
+          <Button variant={'shine'}>Shine</Button>
+          <Button variant={'gooeyRight'}>Gooey Right</Button>
+          <Button variant={'gooeyLeft'}>Gooey Left</Button>
+          <Button variant={'linkHover1'}>Link Hover 1</Button>
+          <Button variant={'linkHover2'}>Link Hover 2</Button>
+        </div>
       </div>
     </>
   );
---
+++ src/lib/ui/typography.tsx
@@ -0,0 +1,3 @@
+export function H1({ children }: { children: string }) {
+  return <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">{children}</h1>;
+}
