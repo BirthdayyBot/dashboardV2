cd3a46a22061b8af9404ddf43e9f67dc6dd2b875
Nikolai Schunk <58025175+nikolaischunk@users.noreply.github.com>
2024-03-04 09:53:56 +0100

    Revert "try: implementation of autoform"
    This reverts commit e9010c61221000e0b470c67fa289bc4f811ee4ac.

--- src/lib/schema/config.tsx
+++ src/lib/schema/config.tsx
@@ -4,22 +4,6 @@ export const TimezoneZodSchema = z.object({
   timezone: z.coerce.number().min(-11, { message: 'Cannot submit a offset lower then -11' }).max(12).default(0),
 });
 
-export const BirthdayChannelZodSchema = () => {
-  // fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     // Create an object where keys are user names and values are user ids
-  //     const userEnum = data.reduce((acc: any, user: any) => {
-  //       acc[user.name] = user.id;
-  //       return acc;
-  //     }, {});
-
-  //     // Create a Zod schema with the userEnum
-  //     const formSchema = z.object({
-  //       user: z.nativeEnum(userEnum),
-  //     });
-  //   });
-  return z.object({
-    birthdayChannel: z.string().optional(),
-  });
-};
+export const BirthdayChannelZodSchema = z.object({
+  birthdayChannel: z.string().optional(),
+});
